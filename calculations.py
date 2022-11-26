d3dd72012310d53d00bc8a7121488d0105d82839
ShivamD <shivamdubey093@gmail.com>
2022-11-26 16:19:20 +0530

    Initial commit

---
+++ calculations.py
@@ -0,0 +1,9 @@
+firstinput=input("FIRST INPUT :")
+secondinput=input("2nd input :")
+characterentry=input("character entry :")
+if int(characterentry)==1:
+    total=int(firstinput)+int(secondinput)
+    
+
+print(total)
+
---
+++ trial 1.py
@@ -0,0 +1,7 @@
+pulse_for_1mm= 2000
+travel_length=int(input("Enter traqvel length in micron :"))
+total_pulses_for_1micron = 2000/1000
+print("Total number of puklses required are = ")
+print(total_pulses_for_1micron*travel_length)
+
+
